af2644bca21ae1bbfaf0342aae00e8a1bdc3ecad
i331795 <rajiv.mucheli@gmail.com>
2025-12-08 08:25:22 +0530

    Add assignment2

---
+++ Assignment2/task1.py
@@ -0,0 +1,10 @@
+# Program to check whether a number is even or odd
+
+# Taking integer input from the user
+num = int(input("Enter an integer: "))
+
+# Checking even or odd
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
---
+++ Assignment2/task2.py
@@ -0,0 +1,10 @@
+# Program to calculate the sum of numbers from 1 to 50
+
+total_sum = 0  # Variable to store the sum
+
+# Using a for loop to iterate from 1 to 50
+for num in range(1, 51):
+    total_sum += num
+
+# Displaying the final sum
+print("The sum of numbers from 1 to 50 is:", total_sum)
